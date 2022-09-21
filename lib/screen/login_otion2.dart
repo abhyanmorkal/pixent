07eac8d67beecca6107e4184aca1458d65199786
Abhyan Morkal <abhyanmorkal@gmail.com>
2022-09-21 11:34:22 +0530

    change som onboarding screen ui

--- lib/screen/login_otion2.dart
+++ lib/screen/login_otion2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login_option_ extends StatefulWidget {
   const Login_option_({super.key});
@@ -29,43 +30,74 @@ class _Login_option_State extends State<Login_option_> {
             'assets/images/emogi.svg',
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height * 1,
-            fit: BoxFit.scaleDown,
+            fit: BoxFit.contain,
           ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 184,
-            decoration: const BoxDecoration(
-              color: Color(0x00FFBE0B),
-            ),
-            child: Center(
-              child: Stack(children: <Widget>[
-                Text(
-                  "PIXENT",
-                  style: TextStyle(
-                    fontSize: 100,
-                    fontFamily: 'FONTH',
-                    // color: Color.fromARGB(255, 255, 190, 11),
-                    foreground: Paint()
-                      ..style = PaintingStyle.stroke
-                      ..strokeWidth = 2
-                      ..color = Colors.black,
+          Column(
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 150,
+                  decoration: const BoxDecoration(
+                    color: Color(0x00FFBE0B),
+                  ),
+                  child: Center(
+                    child: Stack(children: <Widget>[
+                      Text(
+                        "PIXENT",
+                        style: TextStyle(
+                          fontSize: 100,
+                          fontFamily: 'FONTH',
+                          // color: Color.fromARGB(255, 255, 190, 11),
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 2
+                            ..color = Colors.black,
+                        ),
+                      ),
+                      const Text(
+                        "PIXENT",
+                        style: TextStyle(
+                            fontSize: 100,
+                            fontFamily: 'FONTH',
+                            color: Color.fromARGB(255, 255, 190, 11),
+                            shadows: <Shadow>[
+                              Shadow(
+                                color: Colors.black,
+                                offset: Offset(4.0, 4.0),
+                              )
+                            ]),
+                      )
+                    ]),
                   ),
                 ),
-                const Text(
-                  "PIXENT",
-                  style: TextStyle(
-                      fontSize: 100,
-                      fontFamily: 'FONTH',
-                      color: Color.fromARGB(255, 255, 190, 11),
-                      shadows: <Shadow>[
-                        Shadow(
-                          color: Colors.black,
-                          offset: Offset(4.0, 4.0),
-                        )
-                      ]),
-                )
-              ]),
-            ),
+              ),
+              Container(
+                  alignment: AlignmentDirectional.topStart,
+                  padding: EdgeInsets.only(left: 35),
+                  height: 100,
+                  width: MediaQuery.of(context).size.width,
+                  // color: Colors.white,
+                  child: SvgPicture.asset(
+                    'assets/images/Logo.svg',
+                    width: 100,
+                    height: 100,
+                  )),
+              Container(
+                alignment: AlignmentDirectional.topStart,
+                padding: EdgeInsets.symmetric(horizontal: 55),
+                child: Text(
+                    'With the PIXENT, \nyou can download thousands of beautiful wallpapers and create your own personal gallery. You can share them via social media or use them as inspiration for your latest project.',
+                    style: GoogleFonts.rubik(
+                        textStyle: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                            fontStyle: FontStyle.italic))),
+              ),
+            ],
           ),
         ]),
       ),
